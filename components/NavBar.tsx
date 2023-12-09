2b52b1c6f89e266fc35d32c06b03a0129fbc0574
TresorRw <alaintresorcyusa683@gmail.com>
2023-12-09 18:27:09 +0200

    Changed navbar and applied cascade on issues

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -36,13 +36,13 @@ const NavBar = () => {
   }
 
   return (
-    <nav className="navbar shadow-md">
+    <nav className="navbar shadow-md px-4">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52">
+          <ul tabIndex={0} className="menu dropdown-content z-[1] bg-white mt-3 p-2 shadow-md rounded-box w-56">
             <Menus currentPath={currentPath} />
           </ul>
         </div>
