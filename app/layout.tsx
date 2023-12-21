d0876e5760ab69c6455336573d6a27981eed52c9
TresorRw <alaintresorcyusa683@gmail.com>
2023-12-21 11:56:52 +0200

    Added analytics from vercel app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css'
 import { Toaster } from 'react-hot-toast';
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className="">
         {children}
         <SpeedInsights />
+        <Analytics />
         <Toaster position='top-right' />
       </body>
     </html>
